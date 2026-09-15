f330e7054c2422ccac157b0a1d4c829b8fe06842
anastasiabruns <anastasia.bruns2@gmail.com>
2026-09-15 16:16:00 +0200

    CW_260915

--- test/test_BUG.py
+++ test/test_BUG.py
@@ -5,6 +5,13 @@ from faker import Faker
 fake = Faker()
 
 class TestRegistrationBug:
+    @pytest.mark.parametrize("invalid_email", [
+        "vbgyt123.tby.bnj",
+        "bnhjyu78@",
+        "@gmail.com",
+        "fgvty56@@ghyu.vbh",
+        "fgth67 @cvg.bn",
+    ])
     def test_registration_negative_invalid_email_BUG(self, session, registration_url, invalid_email):
         user = User("dfgrt678@gmail", "Qwerty123$")
         body = {
@@ -17,6 +24,13 @@ class TestRegistrationBug:
         assert response.status_code == 400
         assert data["message"]["username"] == "must be a well-formed email address"
 
+    @pytest.mark.parametrize("invalid_password", [
+            "qwerty123$",
+            "QWERTY123!",
+            "Qwerty!$",
+            "Qwerty123",
+            "Ыerty!123",
+        ])
     def test_registration_negative_invalid_password_BUG(self, session, registration_url, invalid_password):
         user = User(fake.email(), "Qwer ty1$")
         body = {
